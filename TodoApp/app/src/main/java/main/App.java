4456a0d9242c34a1d29d24823001e54b2abcf285
Amaral <dmrramaral@gmail.com>
2022-08-04 11:00:40 -0300

    Alteração da MainClass pra iniciar já na tela inicial

--- TodoApp/app/src/main/java/main/App.java
+++ TodoApp/app/src/main/java/main/App.java
@@ -3,18 +3,14 @@
  */
 package main;
 
-
-
 public class App {
+
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-       
-        
-        
-        
-        
+        System.out.println(new App().getGreeting());
+
     }
 }
